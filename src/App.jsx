720e8febddc361907df6fdcaa7c81435eddad068
Vladyslav Natalych <v.natalich05@gmail.com>
2024-10-04 15:15:21 +0300

    Change template

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import './App.css'
 
-import Comp1 from './components/Comp1/Comp1'
-import Comp2 from './components/Comp2/Comp2'
-import Comp3 from './components/Comp3/Comp3'
-
 function App() {
   return (
     <>
-      <Comp1 />
-      <Comp2 />
-      <Comp3/>
+      
     </>
   )
 }
