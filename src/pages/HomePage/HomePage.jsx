5d10069031f7ee56e38d76353ef05aec22e82db0
Vladyslav Natalych <v.natalich05@gmail.com>
2024-10-20 23:52:03 +0300

    Update template

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import Section from '../../components/Section/Section';
+import Container from '../../components/Container/Container';
+
+const HomePage = () => {
+  return (
+    <Section>
+      <Container></Container>
+    </Section>
+  );
+};
+
+export default HomePage;
